f477082e8e65ec113b77c65548c65f37e1dbd746
dajeongg <73213753+dajeongg@users.noreply.github.com>
2026-03-27 17:00:56 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
 
-# GitHub Secrets에서 가져오도록 환경변수 사용
 SMTP_EMAIL = os.environ.get("SMTP_EMAIL")
 SMTP_PASSWORD = os.environ.get("SMTP_PASSWORD")
-OPENAI_KEY = os.environ.get("OPENAI_KEY")
 
-TO_EMAILS = "kimdj0420@gmail.com, kimdajeong@ibk.co.kr"  # 바꾸고 싶으면 직접 수정
-KEYWORD = "은행 or 금융 or AI or 기술"  # 뉴스 검색 키워드
+# 🔥 여러 명 (BCC용)
+TO_EMAILS = [
+    "kimdj0420@gmail.com, kimdajeong@ibk.co.kr"
+]
+
+# 🔥 검색 키워드
+KEYWORD = "은행 OR 금융 OR AI OR 기술"
