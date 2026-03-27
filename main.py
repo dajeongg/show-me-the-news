e4fb995ddaa844803f3dc86a3c74474a55fd4935
dajeongg <73213753+dajeongg@users.noreply.github.com>
2026-03-27 16:05:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fetch_news import fetch_news
-from summarize_news import summarize
+# from summarize_news import summarize
 from send_email import send_email
 
 articles = fetch_news()
 for a in articles:
     content = a["content"] or a["title"]
-    summary = summarize(content)
+  #  summary = summarize(content)
     send_email(a["title"], summary + "\n" + a["url"])
