78a63dedb77f0c55213ca5c7a13b522b20ffda15
dajeongg <73213753+dajeongg@users.noreply.github.com>
2026-03-27 16:42:38 +0900

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ SMTP_EMAIL = os.environ.get("SMTP_EMAIL")
 SMTP_PASSWORD = os.environ.get("SMTP_PASSWORD")
 OPENAI_KEY = os.environ.get("OPENAI_KEY")
 
-TO_EMAIL = "kimdj0420@gmail.com"  # 바꾸고 싶으면 직접 수정
+TO_EMAIL = "kimdj0420@gmail.com and kimdajeong@ibk.co.kr"  # 바꾸고 싶으면 직접 수정
 KEYWORD = "은행 or 금융 or AI or 기술"  # 뉴스 검색 키워드
