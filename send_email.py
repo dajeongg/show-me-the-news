2d67a5f678c94c31d6bb0344af37aca98fb80b46
dajeongg <73213753+dajeongg@users.noreply.github.com>
2026-03-27 16:51:11 +0900

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,12 +1,17 @@
+from config import SMTP_EMAIL, SMTP_PASSWORD, TO_EMAILS
 import smtplib
 from email.mime.text import MIMEText
-from config import SMTP_EMAIL, SMTP_PASSWORD, TO_EMAIL
 
-def send_email(subject, body):
-    msg = MIMEText(body, "plain", "utf-8")
+def send_email(subject, html_body):
+    msg = MIMEText(html_body, "html")
     msg["Subject"] = subject
     msg["From"] = SMTP_EMAIL
-    msg["To"] = TO_EMAIL
+    
+    # 👉 To에는 자기 자신 (형식용)
+    msg["To"] = SMTP_EMAIL
+    
+    # 👉 BCC로 실제 수신자들 숨김 처리
+    msg["Bcc"] = ", ".join(TO_EMAILS)
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
         server.login(SMTP_EMAIL, SMTP_PASSWORD)
