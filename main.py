dd79b58be83bf620b6e85b0d76928b181b06490d
dajeongg <73213753+dajeongg@users.noreply.github.com>
2026-03-27 16:24:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import requests
 from fetch_news import fetch_news
 from send_email import send_email
+
+def shorten_url(url):
+    api = f"http://tinyurl.com/api-create.php?url={url}"
+    return requests.get(api).text
 
 articles = fetch_news()
 
@@ -7,7 +12,7 @@ subject = "오늘의 금융기술 동향"
 body = "📊 오늘의 금융기술 동향\n\n"
 
 for i, a in enumerate(articles, 1):
-    short_url = shorten_url(a["url"])  # 🔥 단축 적용
+    short_url = shorten_url(a["url"])
     body += f"{i}. {a['title']}\n{short_url}\n\n"
 
 send_email(subject, body)
